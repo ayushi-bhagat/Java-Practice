88b913f91849e2947df4cdd4cf8cab37cb3b6632
ayushi-bhagat <ayushibhagat027@gmail.com>
2026-04-04 01:34:14 +0530

    Factorial program
    Java solution for factorial using loop

---
+++ factorial.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        n=sc.nextInt();
+        int fact=1;
+        
+        for(int i=1;i<=n;i++){
+            fact=fact*i;
+        }System.out.println("Factorial is:"+fact);
+    }
+}
